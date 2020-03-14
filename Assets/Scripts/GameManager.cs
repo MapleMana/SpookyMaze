9e695aa9f1054b7fa978ae783821991d03b6b572
ANDREYDEN <andrey2850@gmail.com>
2020-03-14 12:48:11 -0400

    Created a basic setup with 2 scenes, GameManager and UIManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+
+    public static GameManager Instance { get => _instance; }
+
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+        }
+        else if (_instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject wall = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+        wall.transform.position = new Vector3(0, 4, 0);
+        wall.transform.localScale = new Vector3(1, 8, 6);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
