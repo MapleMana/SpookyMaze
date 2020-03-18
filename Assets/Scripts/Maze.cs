8de74a3f9307185a51a797ea0e4e53f53336b762
ANDREYDEN <andrey2850@gmail.com>
2020-03-18 16:35:59 -0400

    Fixed maze generation logic

--- Assets/Scripts/Maze.cs
+++ Assets/Scripts/Maze.cs
@@ -2,6 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public static class ListExtension
+{
+    public static void Shuffle<T>(this List<T> list)
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            int j = Random.Range(0, list.Count);
+            int k = Random.Range(0, list.Count);
+            T value = list[k];
+            list[k] = list[j];
+            list[j] = value;
+        }
+    }
+}
+
 enum WallState
 {
     Destroyed,
@@ -10,7 +25,6 @@ enum WallState
 
 class MazeCell
 {
-    private Vector2Int _pos;
     private Dictionary<Vector2Int, WallState> _wallState;
 
     public static List<Vector2Int> neighbours = new List<Vector2Int> { Vector2Int.up, 
@@ -18,17 +32,14 @@ class MazeCell
                                                                        Vector2Int.down, 
                                                                        Vector2Int.right };
 
-    public MazeCell(int r, int c, WallState top, WallState left, WallState bottom, WallState right)
+    public MazeCell(WallState up, WallState left, WallState down, WallState right)
     {
-        _pos = new Vector2Int(r, c);
         _wallState = new Dictionary<Vector2Int, WallState>();
-        _wallState[Vector2Int.up] = top;
+        _wallState[Vector2Int.up] = up;
         _wallState[Vector2Int.left] = left;
-        _wallState[Vector2Int.down] = bottom;
+        _wallState[Vector2Int.down] = down;
         _wallState[Vector2Int.right] = right;
     }
-
-    public Vector2Int Pos { get => _pos; set => _pos = value; }
 
     public void SetWall(Vector2Int direction, WallState state)
     {
@@ -42,14 +53,15 @@ class MazeCell
 
 public class Maze : MonoBehaviour
 {
-    const float CELL_WIDTH = 1;
-    const float WALL_WIDTH = 0.25f;
+    const float CELL_WIDTH = 10;
+    const float WALL_WIDTH = 2.5f;
 
     public int width;
     public int height;
     public GameObject wallTemplate;
 
     private Dictionary<Vector2Int, MazeCell> _grid;
+    private static Random _generator = new Random();
 
     private void OnEnable()
     {
@@ -69,7 +81,7 @@ public class Maze : MonoBehaviour
         {
             for (int y = 0; y < height; y++)
             {
-                _grid[new Vector2Int(x, y)] = new MazeCell(y, x, wallState, wallState, wallState, wallState);
+                _grid[new Vector2Int(x, y)] = new MazeCell(wallState, wallState, wallState, wallState);
             }
         }
     }
@@ -86,18 +98,17 @@ public class Maze : MonoBehaviour
         _grid[neighbour].SetWall(direction * -1, wallState);
     }
 
+    
+
     public void Generate()
     {
         Fill();
 
         Stack<Vector2Int> path = new Stack<Vector2Int>();
         Dictionary<Vector2Int, bool> visited = new Dictionary<Vector2Int, bool>();
-        for (int r = 0; r < height; r++)
+        foreach (var kvPair in _grid)
         {
-            for (int c = 0; c < width; c++)
-            {
-                visited[new Vector2Int(r, c)] = false;
-            }
+            visited[kvPair.Key] = false;
         }
         Vector2Int start = new Vector2Int(0, 0);
         path.Push(start);
@@ -106,10 +117,9 @@ public class Maze : MonoBehaviour
             Vector2Int curPos = path.Pop();
             MazeCell curCell = _grid[curPos];
 
-            List<Vector2Int> ways = MazeCell.neighbours;
-            ways.Sort((a, b) => Random.value.CompareTo(Random.value));
+            MazeCell.neighbours.Shuffle();
 
-            foreach (Vector2Int direction in ways)
+            foreach (Vector2Int direction in MazeCell.neighbours)
             {
                 Vector2Int newPos = curPos + direction;
                 if (InBounds(newPos) && !visited[newPos])
@@ -135,24 +145,24 @@ public class Maze : MonoBehaviour
     {
         foreach (var kvPair in _grid)
         {
-            MazeCell cell = kvPair.Value;
             Vector2Int pos = kvPair.Key;
-            if (cell.WallExists(Vector2Int.left))
+            MazeCell cell = kvPair.Value;
+            if (cell.WallExists(Vector2Int.right))
             {
-                PutWall(new Vector3(pos.x, 0, -(pos.y + CELL_WIDTH / 2)), false);
+                PutWall(new Vector3(CELL_WIDTH * (pos.x + 1), 0, CELL_WIDTH * (pos.y + 0.5f)), false);
             }
-            if (cell.WallExists(Vector2Int.down))
+            if (cell.WallExists(Vector2Int.up))
             {
-                PutWall(new Vector3(pos.x + CELL_WIDTH / 2, 0, -(pos.y + CELL_WIDTH)), true);
+                PutWall(new Vector3(CELL_WIDTH * (pos.x + 0.5f), 0, CELL_WIDTH * (pos.y + 1)), true);
             }
 
             if (pos.y == 0)
             {
-                PutWall(new Vector3(pos.x + CELL_WIDTH / 2, 0, -pos.y), true);
+                PutWall(new Vector3(CELL_WIDTH * (pos.x + 0.5f), 0, CELL_WIDTH * pos.y), true);
             }
-            if (pos.x == width-1)
+            if (pos.x == 0)
             {
-                PutWall(new Vector3(pos.x + CELL_WIDTH, 0, -(pos.y + CELL_WIDTH / 2)), false);
+                PutWall(new Vector3(CELL_WIDTH * pos.x, 0, CELL_WIDTH * (pos.y + 0.5f)), false);
             }
         }
     }
