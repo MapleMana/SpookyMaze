b21ee2959943fcf49de4b6eb2f4eaa2be648537a
ANDREYDEN <andrey2850@gmail.com>
2020-03-21 15:36:49 -0400

    Added documentation

--- Assets/Scripts/Maze.cs
+++ Assets/Scripts/Maze.cs
@@ -41,11 +41,21 @@ class MazeCell
         _wallState[Vector2Int.right] = right;
     }
 
+    /// <summary>
+    /// Changes the state of a specific wall adjacent to the cell
+    /// </summary>
+    /// <param name="direction">The direction where the wall is located relative to the center of the cell</param>
+    /// <param name="state">The desired wall state</param>
     public void SetWall(Vector2Int direction, WallState state)
     {
         _wallState[direction] = state;
     }
 
+    /// <summary>
+    /// Checks if the wall exists at the specified direction
+    /// </summary>
+    /// <param name="direction">The direction where the wall is located relative to the center of the cell</param>
+    /// <returns></returns>
     public bool WallExists(Vector2Int direction) {
         return _wallState[direction] == WallState.Exists;
     }
@@ -56,41 +66,57 @@ public class Maze : MonoBehaviour
     const float CELL_WIDTH = 10;
     const float WALL_WIDTH = 2.5f;
 
-    public int width;
-    public int height;
+    public int _width = 10;
+    public int _height = 10;
     public GameObject wallTemplate;
 
-    private Dictionary<Vector2Int, MazeCell> _grid;
+    private Dictionary<Vector2Int, MazeCell> _grid = new Dictionary<Vector2Int, MazeCell>();
     private static Random _generator = new Random();
 
-    private void OnEnable()
+    /// <summary>
+    /// Specifies the initial maze state
+    /// </summary>
+    /// <param name="width">The width (X) of the maze</param>
+    /// <param name="height">The height (Z) of the maze</param>
+    public void Initialize(int width, int height)
     {
-        width = 10;
-        height = 10;
-        _grid = new Dictionary<Vector2Int, MazeCell>();
+        _width = width;
+        _height = height;
+
+        Fill();
     }
 
-    void Start()
-    {
-        
-    }
-
+    /// <summary>
+    /// Fills all the walls of the maze with a specific state
+    /// </summary>
+    /// <param name="wallState">The state of the walls to fill with</param>
     private void Fill(WallState wallState=WallState.Exists)
     {
-        for (int x = 0; x < width; x++)
+        for (int x = 0; x < _width; x++)
         {
-            for (int y = 0; y < height; y++)
+            for (int y = 0; y < _height; y++)
             {
                 _grid[new Vector2Int(x, y)] = new MazeCell(wallState, wallState, wallState, wallState);
             }
         }
     }
 
+    /// <summary>
+    /// Checks if the position is in bounds of the maze
+    /// </summary>
+    /// <param name="pos">The position to check</param>
+    /// <returns></returns>
     private bool InBounds(Vector2Int pos)
     {
-        return pos.x >= 0 && pos.x < width && pos.y >= 0 && pos.y < height;
+        return pos.x >= 0 && pos.x < _width && pos.y >= 0 && pos.y < _height;
     }
 
+    /// <summary>
+    /// Changes the state of the specified wall
+    /// </summary>
+    /// <param name="position">The position of the MazeCell that is adjacent to the target wall</param>
+    /// <param name="direction">Determines which wall to change relative to the specified MazeCell</param>
+    /// <param name="wallState">The desired state of the wall</param>
     private void ChangeWall(Vector2Int position, Vector2Int direction, WallState wallState)
     {
         Vector2Int neighbour = position + direction;
@@ -99,11 +125,11 @@ public class Maze : MonoBehaviour
     }
 
     
-
+    /// <summary>
+    /// Generates the maze using the DFS algorithm. All the walls should exist at the start of execution.
+    /// </summary>
     public void Generate()
     {
-        Fill();
-
         Stack<Vector2Int> path = new Stack<Vector2Int>();
         Dictionary<Vector2Int, bool> visited = new Dictionary<Vector2Int, bool>();
         foreach (var kvPair in _grid)
@@ -133,6 +159,11 @@ public class Maze : MonoBehaviour
 
     }
 
+    /// <summary>
+    /// Creates a wall GameObject at the specified position
+    /// </summary>
+    /// <param name="pos">The position to place the wall at</param>
+    /// <param name="horizontal">Determines whether the wall is horizontal or vertical</param>
     private void PutWall(Vector3 pos, bool horizontal=true)
     {
         GameObject wall = Instantiate(wallTemplate, pos, Quaternion.identity);
@@ -141,6 +172,9 @@ public class Maze : MonoBehaviour
         wall.transform.localScale = new Vector3(wallWidth, CELL_WIDTH, wallHeight);
     }
 
+    /// <summary>
+    /// For each MazeCell in _grid creates an actual Wall in 3D space
+    /// </summary>
     public void Display()
     {
         foreach (var kvPair in _grid)
