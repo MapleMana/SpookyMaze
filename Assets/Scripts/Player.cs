bad8fed76a9b112a57a5b1b6f1e59615aa2fa190
Alisa Vynohradova <alice.7414122013@gmail.com>
2020-03-29 17:51:25 -0400

    Added command pattern to Player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,7 +7,11 @@ public class Player : MonoBehaviour
     private static Player _instance;
 
     private Vector2Int _mazePosition;
-    private Vector2Int _movement;
+    private PlayerCommand _command;
+    
+    private List<PlayerCommand> playerCommands = new List<PlayerCommand>();
+    private Coroutine executeRoutine;
+    const float PAUSE_TIME = 0.3f;
 
     public static Player Instance { get => _instance; set => _instance = value; }
 
@@ -27,45 +31,80 @@ public class Player : MonoBehaviour
     {
         _mazePosition = Maze.Instance.start;
     }
+    
+    void Update()
+    {
+        _command = PlayerInputHandler.HandleInput();
+        if (_command != null && executeRoutine == null)
+        {
+            playerCommands.Add(_command);
+            executeRoutine = StartCoroutine(ExecuteCommandsRoutine());
+        }
+    }
 
-    static private Vector2Int GetInput()
+    private IEnumerator ExecuteCommandsRoutine()
+    {
+        playerCommands[playerCommands.Count - 1].Execute(this);
+        MazeCell currentCell = Maze.Instance.Grid[_mazePosition];
+        transform.position = new Vector3(currentCell.cellCenter.x, transform.position.y, currentCell.cellCenter.y);
+        yield return new WaitForSeconds(PAUSE_TIME);
+        executeRoutine = null;
+    }
+
+    public void Move(Vector2Int direction)
+    {
+        if (!Maze.Instance.Grid[_mazePosition].WallExists(direction))
+        {
+            _mazePosition += direction;
+        }
+    }
+}
+
+public class PlayerCommand
+{
+    public PlayerCommand(ExecuteCallback executeMethod)
+    {
+        Execute = executeMethod;
+    }
+
+    public delegate void ExecuteCallback(Player player);
+
+    public ExecuteCallback Execute { get; private set; }
+}
+
+public class PlayerInputHandler
+{
+    private static readonly PlayerCommand MoveUp =
+        new PlayerCommand(delegate (Player player) { player.Move(Vector2Int.up); });
+
+    private static readonly PlayerCommand MoveDown =
+        new PlayerCommand(delegate (Player player) { player.Move(Vector2Int.down); });
+
+    private static readonly PlayerCommand MoveLeft =
+        new PlayerCommand(delegate (Player player) { player.Move(Vector2Int.left); });
+
+    private static readonly PlayerCommand MoveRight =
+        new PlayerCommand(delegate (Player player) { player.Move(Vector2Int.right); });
+
+    public static PlayerCommand HandleInput()
     {
         if (Input.GetAxis("Horizontal") > Mathf.Epsilon)
         {
-            return Vector2Int.right;
+            return MoveRight;
         }
         else if (Input.GetAxis("Horizontal") < -Mathf.Epsilon)
         {
-            return Vector2Int.left;
+            return MoveLeft;
         }
         else if (Input.GetAxis("Vertical") > Mathf.Epsilon)
         {
-            return Vector2Int.up;
+            return MoveUp;
         }
         else if (Input.GetAxis("Vertical") < -Mathf.Epsilon)
         {
-            return Vector2Int.down;
+            return MoveDown;
         }
-        return Vector2Int.zero;
-    }
-
-    void Update()
-    {
-        if (_movement == Vector2Int.zero)
-        {
-            _movement = GetInput();
-            if (_movement != Vector2Int.zero)
-            {
-                if (!Maze.Instance.Grid[_mazePosition].WallExists(_movement))
-                {
-                    _mazePosition += _movement;
-                }
-            }
-        }
-
-        MazeCell currentCell = Maze.Instance.Grid[_mazePosition];
-        transform.position = new Vector3(currentCell.cellCenter.x, transform.position.y, currentCell.cellCenter.y);
-
-        _movement = GetInput();
+        return null;
     }
 }
+
