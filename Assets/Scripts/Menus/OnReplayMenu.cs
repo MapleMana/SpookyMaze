172538cc73c79cf73e2de3b601329c6e6113b7ca
Alisa Vynohradova <56138100+alisondraV@users.noreply.github.com>
2020-06-13 18:39:02 -0400

    Added coroutine stop on Skip event

--- Assets/Scripts/Menus/OnReplayMenu.cs
+++ Assets/Scripts/Menus/OnReplayMenu.cs
@@ -11,7 +11,7 @@ public class OnReplayMenu : Menu<OnReplayMenu>
     /// </summary>
     public void SkipReplay()
     {
-        StopCoroutine("ReplayCommands");
+        LevelManager.Instance.StopAllCoroutines();
         LevelManager.Instance.Clear();
         GameManager.Instance.LoadLevel();
     }
