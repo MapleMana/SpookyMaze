d4ae4c12cfa24b9f9f6fa7ff01d956d09e8de9ae
Alisa Vynohradova <56138100+alisondraV@users.noreply.github.com>
2020-06-20 18:51:47 -0400

    Improved player score calculations

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -25,6 +25,11 @@ public static class LevelGenerator
         return Mathf.FloorToInt(dimensions.Width * dimensions.Height / 2) - 3 * id;
     }
 
+    private static int GetLevelPoints(Dimensions dimensions, int id)
+    {
+        return (dimensions.Width + dimensions.Height) * id;
+    }
+
     public static void GenerateLevels()
     {
         LevelIO.ClearAll();
@@ -46,7 +51,7 @@ public static class LevelGenerator
                                         levelTime: GetLevelTime(mazeDimentions, id),
                                         mode: gameModeName,
                                         mobs: gameMode.GetMovables(),
-                                        levelPoints: i * id)
+                                        levelPoints: GetLevelPoints(mazeDimentions, id))
                     );
 
                     Maze.Instance.Clear();
