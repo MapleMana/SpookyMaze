2873f5a18a059e57471fc463d50f0b52aeed3b72
Maple Mana Studios Inc <45648121+MapleMana@users.noreply.github.com>
2020-07-22 00:22:19 -0400

    Player Character Update
    -Added model for player
    -Added particle effect for torch and fire material
    -Adjusted spot light location to be over torch
    -Changed spot light intensity
    -Added rotation towards target direction to Movable script

--- Assets/Scripts/Agents/Movable.cs
+++ Assets/Scripts/Agents/Movable.cs
@@ -59,6 +59,15 @@ public abstract class Movable : MonoBehaviour
         {
             float dx = Speed * Time.deltaTime;
             transform.position = Vector3.MoveTowards(transform.position, _target, dx);
+
+            // Determine which direction to rotate towards
+            Vector3 targetDirection = transform.position - _target;
+            // The step size is equal to speed times frame time.
+            float singleStep = speed * Time.deltaTime;
+            // Rotate the forward vector towards the target direction by one step
+            Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, singleStep, 0.0f);
+            // Calculate a rotation a step closer to the target and applies rotation to this object
+            transform.rotation = Quaternion.LookRotation(newDirection);
         }
     }
 
