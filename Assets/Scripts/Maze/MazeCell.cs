224c0259229a80bfdd48c3f92774a3228e052f02
Andrii Denysenko <andrey2850@gmail.com>
2020-07-29 00:41:45 -0400

    Removed overlapping corners and made wall height consistent

--- Assets/Scripts/Maze/MazeCell.cs
+++ Assets/Scripts/Maze/MazeCell.cs
@@ -7,13 +7,12 @@ public class MazeCell : System.IDisposable
 {
     public const float CELL_WIDTH = 10;
     public const float WALL_WIDTH = 1.5f;
-    public const float WALL_HEIGHT_INCREMENT = 0.01f;
+    public const float WALL_HEIGHT = 5f;
 
     private Dictionary<Vector2Int, WallState> _wallState = new Dictionary<Vector2Int, WallState>();
     private Vector2Int _position;
     private List<GameObject> _walls;
     private static GameObject _wallTemplate = Resources.Load<GameObject>("Wall");
-    private static float _wallHeight = WALL_WIDTH;
 
     public static List<Vector2Int> neighbours = new List<Vector2Int> { Vector2Int.up,
                                                                        Vector2Int.left,
@@ -24,15 +23,6 @@ public class MazeCell : System.IDisposable
     public GameObject Item { get; set; }
     public bool IsEmpty => ItemType == ItemType.None;
     public Vector2Int Position => _position;
-
-    public static float WallHeight { 
-        get {
-            // this is required for the walls to properly render
-            // no 2 walls should have the same height and overlap
-            _wallHeight += WALL_HEIGHT_INCREMENT;
-            return _wallHeight; 
-        } 
-    }
 
     public Vector3 CellCenter(float y) => new Vector3(CELL_WIDTH * (_position.x + 0.5f), y, CELL_WIDTH * (_position.y + 0.5f));
 
@@ -93,10 +83,10 @@ public class MazeCell : System.IDisposable
     private void PutWall(Vector3 pos, bool horizontal = true)
     {
         GameObject wall = Object.Instantiate(_wallTemplate, pos, Quaternion.identity);
-        float wallX = horizontal ? CELL_WIDTH + WALL_WIDTH : WALL_WIDTH;
-        float wallY = horizontal ? WALL_WIDTH              : CELL_WIDTH + WALL_WIDTH;
+        float wallX = horizontal ? CELL_WIDTH - WALL_WIDTH : WALL_WIDTH;
+        float wallY = horizontal ? WALL_WIDTH              : CELL_WIDTH - WALL_WIDTH;
 
-        wall.transform.localScale = new Vector3(wallX, WallHeight, wallY);
+        wall.transform.localScale = new Vector3(wallX, WALL_HEIGHT, wallY);
         SceneManager.MoveGameObjectToScene(wall, SceneManager.GetSceneByName("Maze"));
         _walls.Add(wall);
     }
