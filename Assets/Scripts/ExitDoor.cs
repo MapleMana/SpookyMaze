079dab4ba287e44a0018e1469bf3629dac8b66df
Maple Mana Studios Inc <45648121+MapleMana@users.noreply.github.com>
2020-09-14 23:28:06 -0400

    Feat: move door to exit
    -added ExitDoor script
    -added function MoveToExit, to move the door to the exit position of the maze
    -added exitDoor reference in LevelManager script and function call to locate exitDoor at the maze exit

---
+++ Assets/Scripts/ExitDoor.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitDoor : MonoBehaviour
+{
+    public void MoveToExit(Maze maze)
+    {
+        MazeCell currentCell = Maze.Instance[maze.EndPos];
+        transform.position = currentCell.CellCenter(y: transform.position.y);
+    }
+}
