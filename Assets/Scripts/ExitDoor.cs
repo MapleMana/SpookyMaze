3a89b115faf7290e0835de9d04a16f490671a7c8
Maple Mana Studios Inc <45648121+MapleMana@users.noreply.github.com>
2020-09-16 23:49:32 -0400

    Feat: Exit door opens
    -exit door now opens when player is close
    -removed unnessary colliders from walls and corners
    -adjusted scale of player and exit door
    -added "Player" layer to Player game object

--- Assets/Scripts/ExitDoor.cs
+++ Assets/Scripts/ExitDoor.cs
@@ -1,12 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class ExitDoor : MonoBehaviour
 {
+    public GameObject door;
+    public Transform rotatePoint;
+
+    private bool openDoor;
+
+    private void Start()
+    {
+        openDoor = true;
+    }
+
+    private void Update()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, 8f, LayerMask.GetMask("Player"));
+        if (hitColliders.Length > 0)
+        {
+            if (openDoor)
+            {
+                door.transform.RotateAround(rotatePoint.transform.position, Vector3.back, 700 * Time.deltaTime);
+                if (Mathf.Abs(TransformUtils.GetInspectorRotation(door.transform).x) > 92.5f)
+                {
+                    openDoor = false;
+                }
+            }            
+        }
+        else
+        {
+            CloseDoor();
+        }
+    }
+
     public void MoveToExit(Maze maze)
     {
+        openDoor = true;
         MazeCell currentCell = Maze.Instance[maze.EndPos];
         transform.position = currentCell.CellCenter(y: transform.position.y);
+    }
+
+    public void CloseDoor()
+    {
+        door.transform.localPosition = new Vector3(0f, 0.1f, 0f);
+        door.transform.localRotation = new Quaternion(0f, 0f, 0f, 0f);
     }
 }
