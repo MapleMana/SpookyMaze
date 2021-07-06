7348d1dffa19e80ef5aa52def983011c7f945f72
Dan Currie <contact@maplemana.com>
2021-07-06 16:46:37 -0400

    WIP #146 & #147
    -added localization for more text
    -added GameCenterManager script

---
+++ Assets/Scripts/Managers/GameCenterManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// https://gamegorillaz.com/blog/game-center-setup-in-unity/
+
+public class GameCenterManager : Singleton<GameCenterManager>
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
