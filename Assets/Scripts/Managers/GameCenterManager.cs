4c07dc991e3a4c8516c070207f6bc747d84d8ce6
Dan Currie <contact@maplemana.com>
2021-07-27 17:34:33 -0400

    WIP #147
    -added game center button to main menu
    -working on game center manager script

--- Assets/Scripts/Managers/GameCenterManager.cs
+++ Assets/Scripts/Managers/GameCenterManager.cs
@@ -6,15 +6,77 @@ using UnityEngine;
 
 public class GameCenterManager : Singleton<GameCenterManager>
 {
-    // Start is called before the first frame update
+    public bool loginSuccessful;
+    public GameObject gameCenterBtn;
+
+    string leaderboardID = "WRITE YOUR LEADERBOARD ID HERE";
+
     void Start()
     {
-        
+        if (Application.platform == RuntimePlatform.IPhonePlayer)
+        {
+            AuthenticateUser();
+            gameCenterBtn.SetActive(true);
+        }        
+        else
+        {
+            gameCenterBtn.SetActive(false);
+        }
+    }    
+
+    void AuthenticateUser()
+    {
+        Social.localUser.Authenticate((bool success) => {
+            if (success)
+            {
+                loginSuccessful = true;
+                Debug.Log("success");
+            }
+            else
+            {
+                Debug.Log("unsuccessful");
+            }
+            // handle success or failure
+        });
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PostScoreOnLeaderBoard(int myScore)
     {
-        
+        if (Application.platform == RuntimePlatform.IPhonePlayer)
+        {
+            if (loginSuccessful)
+            {
+                Social.ReportScore(myScore, leaderboardID, (bool success) => {
+                    if (success)
+                    {
+                        Debug.Log("Successfully uploaded");
+                    }
+                    // handle success or failure
+                });
+            }
+            else
+            {
+                Social.localUser.Authenticate((bool success) => {
+                    if (success)
+                    {
+                        loginSuccessful = true;
+                        Social.ReportScore(myScore, leaderboardID, (bool successful) => {
+                            // handle success or failure
+
+                        });
+                    }
+                    else
+                    {
+                        Debug.Log("unsuccessful");
+                    }
+                    // handle success or failure
+                });
+            }
+        }
+    }
+    
+    public void ShowLeaderboard()
+    {
+        Social.ShowLeaderboardUI();
     }
 }
