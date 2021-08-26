5b4f8e9d6f3dcfa217a73e28da94ed8c91db5ad2
Maple Mana Studios Inc <contact@maplemana.com>
2021-08-25 23:48:23 -0400

    WIP-Btn Blink
    -WIP - added BlinkBtn script to blink the purchase and watch ad to earn coins button
    -ToDo - these buttons should only blink if player hasn't watched an ad in an hour - also, watch ad button is only interactable when blinking

---
+++ Assets/Scripts/utils/BlinkBtn.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BlinkBtn : MonoBehaviour
+{
+    public Image image;
+    public Color imageColourMax;
+    public Color imageColourMin;
+
+    private bool _isBlinking = false;
+    private bool _blinkToMin = true;
+    private float _timeStartedLerping;
+    private float _timeTakenDuringLerp = 0.8f;
+
+    private void Start()
+    {
+        ToggleBlinking(true);
+    }
+
+    void Update()
+    {
+        if (_isBlinking)
+        {
+            if (_blinkToMin)
+            {
+                float timeSinceStarted = Time.time - _timeStartedLerping;
+                float percentComplete = timeSinceStarted / _timeTakenDuringLerp;
+                image.color = Color.Lerp(imageColourMax, imageColourMin, percentComplete);
+                if (percentComplete >= 1.0)
+                {
+                    _blinkToMin = false;
+                    _timeStartedLerping = Time.time;
+                    image.color = imageColourMin;
+                }
+            }
+            else
+            {
+                float timeSinceStarted = Time.time - _timeStartedLerping;
+                float percentComplete = timeSinceStarted / _timeTakenDuringLerp;
+                image.color = Color.Lerp(imageColourMin, imageColourMax, percentComplete);
+                if (percentComplete >= 1.0)
+                {
+                    _blinkToMin = true;
+                    _timeStartedLerping = Time.time;
+                    image.color = imageColourMax;
+                }
+            }
+        }
+    }
+
+    public void ToggleBlinking(bool toggle)
+    {
+        _isBlinking = toggle;
+        _timeStartedLerping = Time.time;
+        if (!toggle)
+        {
+            image.color = imageColourMax;
+        }
+    }
+}
